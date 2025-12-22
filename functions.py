ed40dbe7f09df7472bd0eee4b092fe9d27354eb3
shivam <shivam984861@gmail.com>
2025-12-22 22:50:29 +0530

    initial changes

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 def var_name(a,b):
-    result = a + b
-    print(result)
-    return result
-
+    result1 = a + b
+    result2 = a - b
+    print(result1,result2)
+    return result1,result2
 var_name(5,10)
-
+var_name(10,5)
