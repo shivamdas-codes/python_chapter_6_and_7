ebb3cbb0d878b705b0e970f37a0585827eb794c7
shivam <shivam984861@gmail.com>
2025-12-22 22:55:08 +0530

    initial changes

--- functions.py
+++ functions.py
@@ -1,8 +1,8 @@
-def var_name(a,b):
+def var_name(a,b):  #defination with parameters a and b
     result1 = a + b
     result2 = a - b
-    print(result1,result2)
+    print(result1,result2)  
     return result1,result2
 
-var_name(5,10)
-var_name(10,5)
+var_name(5,10)  #printing the arguments with the given a and b
+var_name(10,5)  #printing the arguments with the given a and b
