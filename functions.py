c19cbb161cb4fb82e2ef08acbe5aa2e131ac507f
shivam <shivam984861@gmail.com>
2025-12-25 15:36:00 +0530

    function added

--- functions.py
+++ functions.py
@@ -11,10 +11,18 @@
 # var_name(10,5)  #printing the arguments with the given a and b
 
 
-# method 2
-# function defination
-def new_var(num1 , num2):   #parameters
-    return num1 + num2
+# # method 2
+# # function defination
+# def new_var(num1 , num2):   #parameters
+#     return num1 + num2
 
-sum = new_var(10,17)    #function call, arguments
-print(sum)
+# sum = new_var(10,17)    #function call, arguments
+# print(sum)
+
+
+# method 3
+def null_value():   #no parametres assigned
+    print("hello")
+
+null_value()    #function call nothing here....
+# this function doesnt return anything so python takes it as "NONE"
