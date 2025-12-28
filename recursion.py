44670d41d5c656febbdca8dc6fd2c8528c093245
shivam <shivam984861@gmail.com>
2025-12-28 19:21:00 +0530

    initial commit

--- recursion.py
+++ recursion.py
@@ -4,7 +4,6 @@ def show(n):    #given parameter as 'n'
         return  #after completion of recursion it will return the value to the "n"
     print(n)
     show(n-1)   #here the numbers will print in desending order.
-
-    show(n-1)
-    print(n)    #here it will print in ascending order.
+    # show(n-1)
+    # print(n)    #here it will print in ascending order.
 show(5) #calling 5 as argument
