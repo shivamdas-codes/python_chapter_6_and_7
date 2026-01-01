e55675ec11a3cb9eb19b66c5d70fc16e005e59a7
shivam <shivam984861@gmail.com>
2026-01-01 12:05:17 +0530

    initial changes

--- recursion.py
+++ recursion.py
@@ -1,15 +1,15 @@
-# # example program:
-# def show(n):    #given parameter as 'n'
+# example program:
+def show(n):    #given parameter as 'n'
 
-#     if n == 0:  #the recursion should perform till "0"                 "this is also known as BASE CASE"             
-#         return  #after completion of recursion it will return the value to the "n"
-#     """ in recursion the BASE CASE is very important if there is no base case then there is a chance that code will get crash """
+    if n == 0:  #the recursion should perform till "0"                 "this is also known as BASE CASE"             
+        return  #after completion of recursion it will return the value to the "n"
+    """ in recursion the BASE CASE is very important if there is no base case then there is a chance that code will get crash """
    
-#     print(n)
-#     show(n-1)   #here the numbers will print in desending order.
-#     # show(n-1)
-#     # print(n)    #here it will print in ascending order.
-# show(5) #calling 5 as argument
+    print(n)
+    show(n-1)   #here the numbers will print in desending order.
+    # show(n-1)
+    # print(n)    #here it will print in ascending order.
+show(5) #calling 5 as argument
 
 
 
