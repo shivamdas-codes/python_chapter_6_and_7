680ee312ed389f0c11e3d568e59a37238bcb5ff2
shivam <shivam984861@gmail.com>
2026-01-02 11:11:20 +0530

    new file added

--- file.py
+++ file.py
@@ -1,6 +1,11 @@
 f = open("file.txt", "r")
-data = f.read()
+
+data = f.read(5)    #the value inside the read() inicates to read the first 5 letters in the text file.
+read_line = f.readline()    #this line indicates to read the first line
+
 print(data)
+print(read_line)
 print(type(data))
 print(len(data))
 f.close()
+
