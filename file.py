ae1ff244b7b0b8f5255896d672714fe7d80a7928
shivam <shivam984861@gmail.com>
2026-01-03 19:15:37 +0530

    new file added

--- file.py
+++ file.py
@@ -1,3 +1,4 @@
+# # reading from a file:
 # f = open("file.txt", "r")
 
 # data = f.read(5)    #the value inside the read() inicates to read the first 5 letters in the text file.
@@ -23,12 +24,15 @@
 # # in the 3rd case the next line will show as empty beacause all the data is already read so there is no data left to read so it will show as empty line in the end.
 
 
-
+# writing to a file:
 file = open("file1.txt","w")
 file.write("hello im intrested in open source......") #this will override the previous data in the file.
 file.write("contributing to real world projects") #this will be added to the previous line without any space or new line.
 file.write("\nhey there how are you") #this will be added to the previous line with a new line.
 file.close()
 
+# appending to a file:
 file = open("sample.txt","a")   #if there is no file with the name sample.txt it will create a new file with that name.
 file.close()
+
+
